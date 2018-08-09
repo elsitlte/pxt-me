1e0b1b443f2b72a0d2e868359a21db738beedb1e
elsitlte <1570056132@qq.com>
2018-08-09 15:17:55 +0800

    third

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@
 namespace me {
 
     //% block
-    export function helloWorld113() {
+    export function helloWorld115() {
 
     }
 
@@ -245,11 +245,11 @@ namespace me {
     }
     
     
-    //% blockId=robotbit_motor_run block="Motor|%index|running with direction|%direction|and speed %speed"
+    //% blockId=robotbit_motor_run block="Motor|%index|speed %speed"
     //% weight=85
     //% speed.min=-255 speed.max=255
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function MotorRun(index: Motors, direction:Directions, speed: number): void {
+    export function MotorRun(index: Motors, direction: Directions, speed: number): void {
         if (!initialized) {
             initPCA9685()
         }
@@ -264,7 +264,7 @@ namespace me {
             return
         let pp = (index - 1) * 2
         let pn = (index - 1) * 2 + 1
-        if (direction > 0) {
+        if (speed >= 0) {
             setPwm(pp, 0, speed)
             setPwm(pn, 0, 0)
         } else {
